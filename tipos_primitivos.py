f6fbc29947b0f023bc5e613ef1586a7b54632a87
Dante-08 <andrehenriquessantos2020@gmail.com>
2024-10-18 17:52:44 -0300

    Add files via upload

---
+++ tipos_primitivos.py
@@ -0,0 +1,73 @@
+# string(str) textos
+
+user_name  = 'André'
+
+# interger (int) numeros inteiros
+
+number_interger1 = 18
+number_interger2 = 20
+
+# float para numeros reais
+
+numero_real1 = 25.7
+numero_real2 = 18.02
+
+# booleano (bool) verdadeiro ou falso
+
+v = True
+f = False
+
+# função - ação
+
+#print ('mostrar algo no console')
+
+numero = 10
+text = '10'
+boleana = True
+real = 5.12
+
+#print(numero)
+
+tipo = type(numero)
+tipo2 = type(text)
+tipo3 = type(boleana)
+tipo4 = type(real)
+
+#print(tipo)
+
+
+# -----------------------------------------
+
+inteiro_em_real = float(numero)
+texto_em_bool = float(text)
+tipo5 = type(inteiro_em_real)
+
+#print(inteiro_em_real)
+#print(texto_em_bool)
+#print(tipo2)
+#print(tipo3)
+#print(tipo4)
+
+#idade = int(input('Digite sua idade: '))
+
+#-----------------------------------------------------------
+n1 = float(input('>> '))
+n2 = int(input('>> '))
+novo_n2 = float(n2)
+soma = n1 + novo_n2
+print(soma)
+
+
+#nome = input('Digite seu nome: ')
+#print(nome)
+
+
+
+
+
+
+
+
+
+
+
