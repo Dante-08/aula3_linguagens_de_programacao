5fc32ddfa1b2244a07d9fe06489f00a4d90eea02
Dante-08 <andrehenriquessantos2020@gmail.com>
2024-10-18 17:54:46 -0300

    Add files via upload

---
+++ operações_aritiméticas.py
@@ -0,0 +1,34 @@
+# soma
+
+print('soma')
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+soma = n1 + n2
+print(soma)
+
+# subtração
+
+print('subtração')
+n3 = int(input('Digite um numero: '))
+n4 = int(input('Digite outro numero: '))
+sub = n3 - n4
+print(sub)
+
+# mutiplicação
+
+print('multiplicação')
+n5 = int(input('Digite um numero: '))
+n6 = int(input('Digite outro numero: '))
+mult = n5 * n6
+print(mult)
+
+# divisão
+
+print('divisão')
+n7 = float(input('Digite um numero: '))
+n8 = float(input('Digite outro numero: '))
+div = n7 / n8
+print(div)
+
+
+print(f'Soma - {soma}')
